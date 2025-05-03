d93a3fe5f55d4c65988e3472f834bc9408ce11ea
TonySl01 <tsleiman111@gmail.com>
2025-05-03 05:29:11 +0300

    finished community

--- Frontend/src/components/ListofPosts/BlogList.jsx
+++ Frontend/src/components/ListofPosts/BlogList.jsx
@@ -1,26 +1,4 @@
-// import React from 'react';
-// import BlogPost from '../Posts/BlogPost';
-// import './BlogList.css';
 
-// const BlogList = ({ isLoading, errors, blogPosts, likePost }) => {
-//   return (
-//     <div className="blog-posts">
-//       {isLoading ? (
-//         <div className="loading-indicator">Loading blog posts...</div>
-//       ) : errors ? (
-//         <div className="error-message">{errors}</div>
-//       ) : blogPosts.length === 0 ? (
-//         <div className="empty-state">No blog posts yet. Be the first to write a blog!</div>
-//       ) : (
-//         blogPosts.map(post => (
-//           <BlogPost key={post.id} post={post} likePost={likePost} />
-//         ))
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BlogList;
 
 import React from 'react';
 import BlogPost from '../Posts/BlogPost';
